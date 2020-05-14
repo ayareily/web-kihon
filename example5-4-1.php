2115b20c1f5e533ac4421a3de397a66492e393ca
ayareily <musiclear05@gmail.com>
2020-05-14 18:56:05 +0900

    変数についての復習

---
+++ example5-4-1.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+    
+<?php
+    $val1 = intval($_POST['val1']);
+    $val2 = intval($_POST['val2']);
+    $result = $val1 + $val2;
+    echo $val1 .'+' .$val2 .'=' . $result;
+?>
+
+</body>
+</html>
