a3f4c53d00ac34590125ec8f4ec8ed67622d5000
ayareily <musiclear05@gmail.com>
2020-05-14 19:15:15 +0900

    if入れ子

--- example5-4-1.php
+++ example5-4-1.php
@@ -2,10 +2,19 @@
 <body>
     
 <?php
-    $val1 = intval($_POST['val1']);
-    $val2 = intval($_POST['val2']);
-    $result = $val1 + $val2;
-    echo $val1 .'+' .$val2 .'=' . $result;
+    if (isset($_POST['val1']) && isset($_POST['val2'])) {
+        $val1 = intval($_POST['val1']);
+        $val2 = intval($_POST['val2']);
+        if (($val1 >= 1) && ($val1 <= 10)) {
+            $result = $val1 + $val2;
+            echo $val1 .'+' .$val2 .'=' . $result;
+        } else {
+        echo '1つめの数字は、1から10までの数字を入力してください';
+        }
+    } else {
+        echo '値を入力してください。<br>';
+        echo "<a href='example5-4-1.html'>入力フォームへ</a>";
+    }
 ?>
 
 </body>
